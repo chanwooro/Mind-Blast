bdcc89408db10f15837b0fbd00023055749bdfde
chanwooro <shcksdn@hotmail.com>
2017-07-30 14:14:52 +1200

    react final setting

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+var gulp = require('gulp');
+var del = require('del');
+var webpack = require('webpack-stream');
+var webpackConfig = require('./webpack.config.js');
+var nodemon = require('gulp-nodemon');
+var path = require('path');
+var less = require('gulp-less');
+
+/**
+ * Build (Webpack)
+ */
+
+gulp.task('clean:build', function() {
+    del('./public/js/*')
+})
+
+gulp.task('build', ['clean:build'], function() {
+  return gulp.src('./app/app.js')
+    .pipe(webpack(webpackConfig))
+    .on('error', function handleError() {
+      this.emit('end'); // Recover from errors
+    })
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('watch:build', function() {
+  return gulp.watch('./app/**/*', ['build']);
+});
+
+gulp.task('less', function(){
+  return gulp.src('./less/app.less')
+    .pipe(less())
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('watch:less', function() {
+  return gulp.watch('./less/**/*.less', ['less']);
+});
+
+/**
+ * Node Server (Express)
+ */
+
+gulp.task('serve:node', function(done) {
+  nodemon({
+    exec: 'node ./node_modules/babel-cli/bin/babel-node.js ./server.js',
+    watch: ['server.js'],
+    ext: 'js html'
+  });
+});
+
+
+/**
+ * Main tasks
+ */
+
+gulp.task('serve', ['serve:node']);
+gulp.task('watch', ['build', 'watch:build', 'less', 'watch:less']);
+gulp.task('default', ['serve']);
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,18 @@
 var path = require('path');
-var webpack = require('webpack');
 
 module.exports = {
- entry: './js/app.js',
- output: {
-  path: __dirname,
-  filename: 'js/bundle.js'
- },
- watch: true,
- module: {
-  loaders: [
-   {
-    test: /.jsx?$/,
-    loader: 'babel-loader',
-    exclude: /node_modules/,
-    query: {
-     presets: ['es2015', 'react']
+    entry: "./app/app.js",
+    output: {
+        filename: "public/js/bundle.js",
+        sourceMapFilename: "public/js/bundle.map"
+    },
+    devtool: '#source-map',
+    module: {
+        loaders: [
+            {
+                loader: 'babel',
+                exclude: /node_modules/
+            }
+        ]
     }
-   }
-
-  ]
- }, 
-};
+}
